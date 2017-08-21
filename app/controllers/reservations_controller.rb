89874ae2ab3a395d7515ddeade93044928e54d51
Julian Compagni Portis <jcompagni@gmail.com>
2017-08-21 03:17:53 -0700

    remove index page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,16 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
-  # GET /reservations
-  # GET /reservations.json
-  def index
-    @reservations = Reservation.all
-  end
-
-  # GET /reservations/1
-  # GET /reservations/1.json
-  def show
-  end
 
   # GET /reservations/new
   def new
@@ -21,9 +11,6 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new    
   end
 
-  # GET /reservations/1/edit
-  def edit
-  end
 
   #show day-of rental form
   def hideResDate
@@ -91,29 +78,7 @@ class ReservationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reservations/1
-  # PATCH/PUT /reservations/1.json
-  def update
-    respond_to do |format|
-      if @reservation.update(reservation_params)
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @reservation }
-      else
-        format.html { render :edit }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  # DELETE /reservations/1
-  # DELETE /reservations/1.json
-  def destroy
-    @reservation.destroy
-    respond_to do |format|
-      format.html { redirect_to reservations_url, notice: 'Reservation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   # get all reserved dates from database
   def get_reserved_dates
